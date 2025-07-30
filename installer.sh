4c65273cc104bc11f45597c394c19c6db231b6b7
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-07-30 23:09:26 +0530

    Added Installer

---
+++ installer.sh
@@ -0,0 +1,20 @@
+FASTFETCH_DIR="/home/$USER/.config/fastfetch"
+if ! [[ -d "$FASTFETCH_DIR" ]]; then
+    echo "Creating Fastfetch directory..."
+    mkdir -p "$FASTFETCH_DIR"
+else
+    echo "Fastfetch directory already exists."
+    echo "Backing up existing Fastfetch configuration files..."
+    a=$(date +%Y%m%d_%H%M%S)
+    mkdir -p "$FASTFETCH_DIR/backup_$a"
+    cp -r "$FASTFETCH_DIR"/* "$FASTFETCH_DIR/backup_$a/"
+    echo "Backup completed."
+fi
+
+echo "Copying Fastfetch configuration files..."
+cp -r ./fastfetch/* "$FASTFETCH_DIR/"
+
+echo "Fastfetch configuration files copied successfully."
+echo "Initializing Fastfetch Script..."
+
+exec "$FASTFETCH_DIR/fastfetch-scripts.sh --setup"
